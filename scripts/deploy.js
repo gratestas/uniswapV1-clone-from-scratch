b0bdd55806b47e42e85ae4aedfed9c3f12f2de70
Stanislav Grate <stasgrate@gmail.com>
2022-02-17 20:08:25 +0300

    added script to save contract address and abi programatically

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,3 +1,5 @@
+const fs = require("fs");
+
 const main = async () => {
   // We get the contract to deploy
   const Factory = await ethers.getContractFactory("Factory");
@@ -6,6 +8,15 @@ const main = async () => {
   await factory.deployed();
 
   console.log("Factory.sol deployed to:", factory.address);
+
+  const data = {
+    address: factory.address,
+    abi: JSON.parse(factory.interface.format("json")),
+  };
+  fs.writeFileSync(
+    "client/smart_contract/data/Factory.json",
+    JSON.stringify(data)
+  );
 };
 
 // We recommend this pattern to be able to use async/await everywhere
