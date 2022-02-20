efa9ec9ad29b7b3c18ea3e21d06789510b072e2c
Stanislav Grate <stasgrate@gmail.com>
2022-02-20 16:26:45 +0300

    refactored components

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -1,10 +1,11 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useContext } from 'react'
 import { FiArrowUpRight } from 'react-icons/fi'
 import { AiOutlineDown } from 'react-icons/ai'
 import { HiOutlineDotsVertical } from 'react-icons/hi'
 import ethLogo from '../assets/eth.png'
-import { useContext } from 'react'
 import Image from 'next/image'
+
+import { TransactionContext } from '../context/TransactionContext'
 
 const navItems = ['Swap', 'Pool', 'Vote', 'Charts']
 const styles = {
@@ -18,12 +19,21 @@ const styles = {
   button: `flex items-center mx-2 rounded-2xl bg-[#191B1F] text-[0.9rem] font-semibold cursor-pointer`,
   buttonPadding: `p-1`,
   buttonIconContainer: `flex items-center justify-center w-8 h-8`,
-  buttonTextContainer: `flex items-center h-8`,
+  buttonTextContainer: `flex items-center h-8 px-2`,
   buttonHighlight: `flex items-center justify-center h-full text-[#4F90EA] bg-[#172A42] border border-[#163256] hover:border-[#234169]  rounded-2xl`,
 }
 
 const Header = () => {
   const [activeNav, setActiveNav] = useState('Swap')
+  const { currentAccount, connectWallet } = useContext(TransactionContext)
+  const [userName, setUserName] = useState()
+
+  useEffect(() => {
+    if (!currentAccount) return
+    setUserName(`${currentAccount.slice(0, 6)}...${currentAccount.slice(38)}`)
+  }, [currentAccount])
+  console.log(currentAccount, connectWallet)
+
   return (
     <header className={styles.wrapper}>
       <div className={styles.headerLogo}>Muuswap</div>
@@ -54,15 +64,22 @@ const Header = () => {
             <AiOutlineDown />
           </div>
         </div>
-
-        <div
-          className={`${styles.button} ${styles.buttonPadding}`}
-          onClick={() => connectWallet()}
-        >
-          <div className={`${styles.buttonHighlight} ${styles.buttonPadding}`}>
-            Connect Wallet
+        {currentAccount ? (
+          <div className={`${styles.button} ${styles.buttonPadding}`}>
+            <div className={styles.buttonTextContainer}>{userName}</div>
           </div>
-        </div>
+        ) : (
+          <div
+            className={`${styles.button} ${styles.buttonPadding}`}
+            onClick={() => connectWallet()}
+          >
+            <div
+              className={`${styles.buttonHighlight} ${styles.buttonPadding}`}
+            >
+              Connect Wallet
+            </div>
+          </div>
+        )}
 
         <div className={`${styles.button} ${styles.buttonPadding}`}>
           <div className={`${styles.buttonIconContainer} mx-1`}>
