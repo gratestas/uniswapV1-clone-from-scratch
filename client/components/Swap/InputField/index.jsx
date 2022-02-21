14100a3ce1edfeb00ea1b1a14a554b52c4a41de8
Stanislav Grate <stasgrate@gmail.com>
2022-02-21 22:19:51 +0300

    wip: implementing swap page

--- client/components/Swap/InputField/index.jsx
+++ client/components/Swap/InputField/index.jsx
@@ -1,23 +1,26 @@
 import CurrencySelectButton from '../../CurrencySelectButton/index.jsx'
 
 const styles = {
-  transactionInputContainer: `bg-[#20242A] my-3 rounded-2xl p-6 text-3xl  border border-[#20242A] hover:border-[#41444F]  flex justify-between`,
-  transactionInput: `bg-transparent placeholder:text-[#B2B9D2] outline-none mb-6 w-full text-2xl`,
+  inputContainer: `bg-[#20242A] my-3 rounded-2xl p-6 text-3xl  border border-[#20242A] hover:border-[#41444F]  flex justify-between`,
+  input: `bg-transparent placeholder:text-[#B2B9D2] outline-none mb-6 w-full text-2xl`,
 }
 
-const InputField = () => {
+const InputField = ({ selectedToken, tradeSide }) => {
   const handleChange = (e, amount) => {}
 
   return (
-    <div className={styles.transactionInputContainer}>
+    <div className={styles.inputContainer}>
       <input
         type="text"
-        className={styles.transactionInput}
+        className={styles.input}
         placeholder="0.0"
         pattern="[0-9]*[.,]?[0-9]*$"
         onChange={(e) => handleChange(e, 'amount')}
       />
-      <CurrencySelectButton />
+      <CurrencySelectButton
+        selectedToken={selectedToken}
+        tradeSide={tradeSide}
+      />
     </div>
   )
 }
--- client/components/Swap/Panel/index.jsx
+++ client/components/Swap/Panel/index.jsx
@@ -1,22 +1,26 @@
+import { useContext } from 'react'
 import { RiSettings3Fill } from 'react-icons/ri'
+import CurrencyListModal from '../../CurrencyListModal/index.jsx'
 
-import CurrencySelectButton from '../../CurrencySelectButton/index.jsx'
+import { TransactionContext } from '../../../context/TransactionContext'
 import InputField from '../InputField/index.jsx'
 import { styles } from './styles'
 
 const SwapPanel = () => {
+  const { tokenPair } = useContext(TransactionContext)
   const handleSubmit = (e) => {}
   return (
     <div className={styles.swapForm}>
+      <CurrencyListModal />
       <div className={styles.formHeader}>
         <div>Swap</div>
         <div>
           <RiSettings3Fill />
         </div>
       </div>
-      <InputField />
-      <InputField />
-      <div className={styles.confirButton} onClick={(e) => handleSubmit(e)}>
+      <InputField selectedToken={tokenPair.in} tradeSide="in" />
+      <InputField selectedToken={tokenPair.out} tradeSide="out" />
+      <div className={styles.confirmButton} onClick={(e) => handleSubmit(e)}>
         Confirm
       </div>
     </div>
