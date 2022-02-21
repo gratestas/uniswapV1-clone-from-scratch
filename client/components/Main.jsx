8ee81d4af4ad88af0066cbc47c41d8c69f9601ef
Stanislav Grate <stasgrate@gmail.com>
2022-02-21 22:35:10 +0300

    deleted styles object

--- client/components/Main.jsx
+++ client/components/Main.jsx
@@ -1,13 +1,8 @@
-import { RiSettings3Fill } from 'react-icons/ri'
-
 import SwapPanel from '../components/Swap/Panel/index.jsx'
-const styles = {
-  wrapper: `w-screen flex items-center justify-center mt-14`,
-}
 
 const Main = () => {
   return (
-    <main className={styles.wrapper}>
+    <main>
       <SwapPanel />
     </main>
   )
