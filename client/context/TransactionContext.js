3417e2b4afcc78a0b8fa6ded87f7900c265992bc
Stanislav Grate <stasgrate@gmail.com>
2022-02-21 22:41:05 +0300

    added state variables necessary for updating token pairs being selected in currency list component

--- client/context/TransactionContext.js
+++ client/context/TransactionContext.js
@@ -1,5 +1,9 @@
 import { useState, useEffect, createContext } from 'react'
-
+import {
+  getFactoryContract,
+  createExchange,
+  getTokenContract,
+} from '../smart_contract/lib/utils'
 export const TransactionContext = createContext()
 
 let eth
@@ -8,24 +12,89 @@ if (typeof window !== 'undefined') {
   eth = window.ethereum
 }
 
-const TransactionProvider = ({ children }) => {
-  const [currentAccount, setCurrentAccount] = useState()
+const isMetamaskInstalled = (metamask = eth) => {
+  if (!metamask) return alert('Please install metamask extention!')
+}
 
-  const connectWallet = (metamask = eth) => {
+export const TransactionProvider = ({ children }) => {
+  const [currentAccount, setCurrentAccount] = useState()
+  //const [selectedTokenIn, setSelectedTokenIn] = useState()
+  //const [selectedTokenOut, setSelectedTokenOut] = useState()
+  const [tokenPair, setTokenPair] = useState({
+    in: '',
+    out: '',
+  })
+  const [tradeSide, setTradeSide] = useState('')
+  const [isCurrencyListOpen, setIsCurrencyListOpen] = useState(false)
+  useEffect(() => {
+    checkIfWalletConnected()
+  }, [])
+
+  const connectWallet = async (metamask = eth) => {
     try {
-      if (!metamask) return alert('Please install metamask extention!')
-      ;[mainAccount] = await metamask.request({ method: 'eth_requestAccounts' })
-      setCurrentAccount(mainAccount)
+      isMetamaskInstalled(metamask)
+      const [connectedAccount] = await metamask.request({
+        method: 'eth_requestAccounts',
+      })
+      setCurrentAccount(connectedAccount)
     } catch (error) {
       console.error(error)
       throw new Error('No ethereum object')
     }
   }
+
+  const checkIfWalletConnected = async (metamask = eth) => {
+    try {
+      isMetamaskInstalled(metamask)
+      const accounts = await metamask.request({ method: 'eth_accounts' })
+      if (accounts.length) {
+        setCurrentAccount(accounts[0])
+      }
+      const contract = getFactoryContract(metamask)
+      console.log({ contract })
+      //const exchange = await createExchange(metamask)
+      //console.log({ exchange })
+      //
+      //const token = getTokenContract(metamask)
+      //console.log({ token })
+    } catch (error) {
+      console.error(error)
+      throw new Error('No ethereum object')
+    }
+  }
+
+  // Checks if wallet is connected to the correct network
+  const checkCorrectNetwork = async () => {
+    let chainId = await ethereum.request({ method: 'eth_chainId' })
+    console.log('Connected to chain:' + chainId)
+
+    const rinkebyChainId = '0x4'
+
+    if (chainId !== rinkebyChainId) {
+      setCorrectNetwork(false)
+    } else {
+      setCorrectNetwork(true)
+    }
+  }
+
+  const closePanel = () => {
+    setIsCurrencyListOpen(false)
+  }
   return (
-    <TransactionContext.Provider value={{ currentAccount, connectWallet }}>
+    <TransactionContext.Provider
+      value={{
+        currentAccount,
+        connectWallet,
+        isCurrencyListOpen,
+        setIsCurrencyListOpen,
+        tokenPair,
+        setTokenPair,
+        tradeSide,
+        setTradeSide,
+        closePanel,
+      }}
+    >
       {children}
     </TransactionContext.Provider>
   )
 }
-
-export default TransactionProvider
