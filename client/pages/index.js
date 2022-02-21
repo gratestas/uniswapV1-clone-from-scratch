ce9d24957e46739444fc743219f3f0657f68da78
Stanislav Grate <stasgrate@gmail.com>
2022-02-21 22:36:30 +0300

    refactored component

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,11 +1,5 @@
 import Head from 'next/head'
-
-import Header from '../components/Header.jsx'
 import Main from '../components/Main.jsx'
-
-const styles = {
-  wrapper: `h-screen max-h-screen h-min-screen w-screen bg-[#2D242F] text-white select-none flex flex-col justify-between`,
-}
 
 const Home = () => {
   return (
@@ -14,12 +8,7 @@ const Home = () => {
         <title>Muuswap: Decentralized Exchange</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main className={styles.wrapper}>
-        <Header />
-        <Main />
-        <h2>footer</h2>
-      </main>
+      <Main />
     </div>
   )
 }
