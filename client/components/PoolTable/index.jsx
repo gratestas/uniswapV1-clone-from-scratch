91b8a78236924a20904f5634fbff4064226f75d6
gratestas <stasgrate@gmail.com>
2022-03-04 13:50:34 +0300

    created table containing data related to liquidity pools

---
+++ client/components/PoolTable/index.jsx
@@ -0,0 +1,56 @@
+import Image from 'next/image'
+import ethLogo from '../../assets/eth.png'
+import { styles } from './styles'
+
+const theadItems = ['Pool', 'TVL', 'Volume 24H', 'Volume 7D']
+
+const PoolTable = ({ poolsData }) => {
+  return (
+    <div className={styles.container}>
+      <table className={styles.table}>
+        <thead className={styles.thead}>
+          <tr className="hover:bg-">
+            {theadItems.map((item, index) => (
+              <th key={index} scope="col" className={styles.thead_item}>
+                {item}
+              </th>
+            ))}
+          </tr>
+        </thead>
+        <tbody className={styles.tbody}>
+          {poolsData &&
+            poolsData.map((pool) => (
+              <tr key={pool.token.name} className={styles.tbody_row}>
+                <td scope="col" className={styles.tbody_item}>
+                  <div className={styles.tbody_item__inner}>
+                    <div className="mr-4 h-8 w-8">
+                      <Image
+                        src={pool.token.icon}
+                        alt={pool.token.symbol}
+                        fill="contain"
+                      />
+                    </div>
+                    <div className="absolute left-6 h-8 w-8">
+                      <Image src={ethLogo} alt="eth" fill="contain" />
+                    </div>
+                    <div className="ml-6">{pool.token.symbol}-ETH</div>
+                  </div>
+                </td>
+                <td scope="col" className={styles.tbody_item}>
+                  {pool.liquidity} ETH
+                </td>
+                <td scope="col" className={styles.tbody_item}>
+                  $--
+                </td>
+                <td scope="col" className={styles.tbody_item}>
+                  $--
+                </td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+export default PoolTable
---
+++ client/components/PoolTable/styles.js
@@ -0,0 +1,10 @@
+export const styles = {
+  container: `h-96 w-[90rem] rounded-2xl bg-[#191B1F] px-8 py-4`,
+  table: `table-auto divide-y divide-gray-700 w-full`,
+  thead: ``,
+  thead_item: `first:text-left text-right first:w-[40rem] py-3 mx-6 text-gray-400 font-normal text-lg tracking-wide`,
+  tbody: `divide-y divide-gray-800`,
+  tbody_row: `my-2 mt-2 text-center hover:text-[#f0aa12c5]`,
+  tbody_item: `first:text-left text-right py-4 font-medium tracking-wider text-lg `,
+  tbody_item__inner: `relative flex items-center`,
+}
