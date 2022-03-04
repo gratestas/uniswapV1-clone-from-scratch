87e2f23c498d9d372d9224ab91a7853dba1d67c5
gratestas <stasgrate@gmail.com>
2022-03-04 14:01:33 +0300

    refactored

--- client/pages/liquidityPool.js
+++ client/pages/liquidityPool.js
@@ -1,29 +1,98 @@
 import { useContext, useState, useEffect } from 'react'
 import CurrencyListModal from '../components/CurrencyListModal'
-import InputField from '../components/Swap/InputField'
+import CurrencySelectButton from '../components/CurrencySelectButton'
+import LiquidityPosition from '../components/Liquidity/LiquidityPosition'
 import { TransactionContext } from '../context/TransactionContext'
+import { WalletContext } from '../context/WalletContext'
+import { getLPTokensAndPoolShare } from '../smart_contract/lib/liquidity'
+import {
+  addLiquidity,
+  getExchange,
+} from '../smart_contract/lib/contractFunctions'
+import {
+  fromWei,
+  getBalance,
+  getSigner,
+  toWei,
+  formatPrecision,
+} from '../smart_contract/lib/utils'
 
 const styles = {
-  wrapper: `flex flex-col space-y-8`,
+  wrapper: `flex flex-col `,
   container: `bg-[#191B1F] w-[40rem] rounded-2xl p-4`,
-  header: `px-2 flex items-center justify-center font-medium text-xl pb-2 border-b border-gray-800`,
-  fieldTitle: `mt-6 text-sm tracking-wide`,
-  liquidityContainer: `flex justify-between`,
-  reserves: `flex flex-col items-end mr-4`,
-  confirmButton: `bg-[#2172E5] my-2 rounded-2xl py-6 px-8 text-xl font-semibold flex items-center justify-center cursor-pointer border border-[#2172E5] hover:border-[#234169]`,
+  header: `px-2 mb-6 flex items-center justify-center font-medium text-xl pb-2 border-b border-gray-800`,
+  fieldTitle: `text-sm tracking-wide`,
+  liquidityContainer: `py-4 flex justify-between border-t border-gray-800`,
+  reserves: `flex flex-col items-end mr-4 text-sm font-medium tracking-wide`,
+  inputContainer: `bg-[#20242A] my-3 rounded-2xl p-6 text-3xl  border border-[#20242A] hover:border-[#41444F]  flex justify-between`,
+  input: `bg-transparent placeholder:text-[#B2B9D2] outline-none mb-6 w-full text-2xl`,
+  confirmButton: `bg-[#2172E5] my-4 rounded-2xl py-6 px-8 text-xl font-semibold flex items-center justify-center cursor-pointer border border-[#2172E5] hover:border-[#234169]`,
 }
 
-const TRADE_SIDE = { in: 'in', out: 'out' }
-
 const LiquidityPool = () => {
-  const { tokenPair, setTokenPair } = useContext(TransactionContext)
-  const [fieldValueIn, setFieldValueIn] = useState()
-  const [fieldValueOut, setFieldValueOut] = useState()
+  const { factory, tokenPair, setTokenPair } = useContext(TransactionContext)
+  const { currentAccount } = useContext(WalletContext)
+  const [input, setInput] = useState('')
+  const [output, setOutput] = useState('')
+  const [tokenReserve, setTokenReserve] = useState()
+  const [ethReserve, setEthReserve] = useState()
+  const [LPtokens, setLPtokens] = useState(null)
+  const [poolShare, setPoolShare] = useState(0)
+  console.log(' currentAccount', currentAccount)
 
-  const handleSubmit = (e) => {}
-  useEffect(() => {
-    setTokenPair({ in: '', out: '' })
-  }, [])
+  const fetchLPTokensAndPoolShare = async (tokenAddress, currentAccount) => {
+    const [LP_tokens, poolShare] = await getLPTokensAndPoolShare(
+      tokenAddress,
+      currentAccount
+    )
+    setLPtokens(LP_tokens)
+    setPoolShare(poolShare)
+  }
+
+  const handleSubmit = async () => {
+    const signer = getSigner(window.ethereum)
+    try {
+      if (!(await factory.doesExchangeExist(tokenPair.in.address))) {
+        console.log(
+          'there is no exchange registered by provided address. creating new exchange...'
+        )
+        await createExchange(tokenPair.in.address, signer)
+      }
+    } catch (error) {
+      consolo.log(error.message)
+    }
+    await addLiquidity(
+      tokenPair.in.address,
+      toWei(Number(input)),
+      toWei(Number(output)),
+      signer
+    )
+
+    await fetchLPTokensAndPoolShare(tokenPair.in.address, currentAccount)
+
+    console.log('your share in pool:', poolShare)
+  }
+
+  useEffect(async () => {
+    if (!tokenPair.in) return
+
+    const signer = getSigner(window.ethereum)
+    const exchange = await getExchange(tokenPair.in.address, signer)
+
+    const tokenReserve = await exchange.getReserve()
+    const formattedTokenReserve = formatPrecision(fromWei(tokenReserve))
+    setTokenReserve(formattedTokenReserve)
+
+    const ethRerserve = await getBalance(exchange.address)
+    const formattedEthReserve = formatPrecision(fromWei(ethRerserve))
+    setEthReserve(formattedEthReserve)
+
+    await fetchLPTokensAndPoolShare(tokenPair.in.address, currentAccount)
+  }, [tokenPair.in.address, currentAccount])
+
+  //useEffect(() => {
+  //  setTokenPair({ in: '', out: '' })
+  //}, [])
   return (
     <div className={styles.wrapper}>
       <CurrencyListModal />
@@ -31,37 +100,56 @@ const LiquidityPool = () => {
         <div className={styles.header}>Add Liquidity</div>
         <div>
           <div className={styles.fieldTitle}>Deposit Amount</div>
-          <InputField
-            selectedToken={tokenPair.in}
-            tradeSide={TRADE_SIDE.in}
-            setFieldValue={setFieldValueIn}
-          />
-          <InputField
-            selectedToken={tokenPair.out}
-            tradeSide={TRADE_SIDE.out}
-            setFieldValue={setFieldValueOut}
-          />
+          <div className={styles.inputContainer}>
+            <input
+              type="number"
+              value={input}
+              className={styles.input}
+              placeholder="0.0"
+              pattern="^[+]?([0-9]+(?:[\.][0-9]*)?|\.[0-9]+)$"
+              onChange={(e) => {
+                setInput(e.target.value)
+              }}
+            />
+            <CurrencySelectButton selectedToken={tokenPair.in} tradeSide="in" />
+          </div>
+          <div className={styles.inputContainer}>
+            <input
+              type="number"
+              className={styles.input}
+              value={output}
+              placeholder="0.0"
+              pattern="^[+]?([0-9]+(?:[\.][0-9]*)?|\.[0-9]+)$"
+              onChange={(e) => setOutput(e.target.value)}
+            />
+            <CurrencySelectButton
+              selectedToken={tokenPair.out}
+              tradeSide="out"
+              disabled
+            />
+          </div>
           <div className={styles.liquidityContainer}>
             <div className={styles.fieldTitle}>Pool Liquidity</div>
             <div className={styles.reserves}>
-              <div>12122121 Token1</div>
-              <div>4349 Token2</div>
+              <div className="mb-1 tracking-wider">
+                {tokenReserve} &nbsp; {tokenPair.in.symbol}
+              </div>
+              <div className="tracking-wider">{ethReserve} &nbsp; ETH</div>
             </div>
           </div>
           <div
             className={styles.confirmButton}
             onClick={(e) => handleSubmit(e)}
           >
-            Confirm
+            Supply
           </div>
         </div>
       </div>
-      <div className={styles.container}>
-        <div className={styles.header}>Your Liquidity</div>
-        <div>
-          <div className={styles.fieldTitle}>You have no position</div>
-        </div>
-      </div>
+      <LiquidityPosition
+        token={tokenPair.in}
+        LPtokens={LPtokens}
+        poolShare={poolShare}
+      />
     </div>
   )
 }
