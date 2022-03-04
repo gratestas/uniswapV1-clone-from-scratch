7de9b91d81c977bcf2c7fad39ed1952522c2219c
gratestas <stasgrate@gmail.com>
2022-03-04 18:57:48 +0300

    moved styles to a separate file

--- client/pages/liquidity.js
+++ client/pages/liquidity.js
@@ -16,18 +16,7 @@ import {
   toWei,
   formatPrecision,
 } from '../smart_contract/lib/utils'
-
-const styles = {
-  wrapper: `flex flex-col `,
-  container: `bg-[#191B1F] w-[40rem] rounded-2xl p-4`,
-  header: `px-2 mb-6 flex items-center justify-center font-medium text-xl pb-2 border-b border-gray-800`,
-  fieldTitle: `text-sm tracking-wide`,
-  liquidityContainer: `py-4 flex justify-between border-t border-gray-800`,
-  reserves: `flex flex-col items-end mr-4 text-sm font-medium tracking-wide`,
-  inputContainer: `bg-[#20242A] my-3 rounded-2xl p-6 text-3xl  border border-[#20242A] hover:border-[#41444F]  flex justify-between`,
-  input: `bg-transparent placeholder:text-[#B2B9D2] outline-none mb-6 w-full text-2xl`,
-  confirmButton: `bg-[#2172E5] my-4 rounded-2xl py-6 px-8 text-xl font-semibold flex items-center justify-center cursor-pointer border border-[#2172E5] hover:border-[#234169]`,
-}
+import { styles } from '../styles/liquidity'
 
 const LiquidityPool = () => {
   const { factory, tokenPair, setTokenPair } = useContext(TransactionContext)
