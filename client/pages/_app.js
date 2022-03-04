26339a4a1391f9073218e08c96a04a7499ab7398
gratestas <stasgrate@gmail.com>
2022-03-04 13:54:11 +0300

    imported font external links

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -3,16 +3,33 @@ import { WalletProvider } from '../context/WalletContext'
 import Layout from '../components/shared/Layout'
 
 import '../styles/globals.css'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <WalletProvider>
-      <TransactionProvider>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </TransactionProvider>
-    </WalletProvider>
+    <div>
+      <Head>
+        <title>My App</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com" key="font" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+          key="font"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <WalletProvider>
+        <TransactionProvider>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </TransactionProvider>
+      </WalletProvider>
+    </div>
   )
 }
 
