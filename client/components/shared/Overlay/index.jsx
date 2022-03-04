cfa688e1f4333a9dfed69b51907e6dabe41540bc
gratestas <stasgrate@gmail.com>
2022-03-04 13:39:35 +0300

    changed opacity

--- client/components/shared/Overlay/index.jsx
+++ client/components/shared/Overlay/index.jsx
@@ -1,6 +1,6 @@
 const Overlay = () => {
   return (
-    <div className="absolute inset-0 bg-slate-900/40 transition duration-500 "></div>
+    <div className="absolute inset-0 bg-slate-900/70 transition duration-500 "></div>
   )
 }
 
