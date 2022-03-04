80389a369435573ef96d750d5d9ed3f8a72e70cf
gratestas <stasgrate@gmail.com>
2022-03-04 18:46:41 +0300

    moved styles to separate file

--- client/components/Liquidity/LiquidityPosition/index.jsx
+++ client/components/Liquidity/LiquidityPosition/index.jsx
@@ -8,35 +8,31 @@ const LuqiudityPosition = ({ token, LPtokens, poolShare }) => {
     <div className={`${styles.container} mt-10`}>
       <div className={styles.header}>Your Liquidity</div>
       {LPtokens ? (
-        <div className="my-3 flex justify-between rounded-2xl border border-[#20242A]  bg-[#20242A] p-6 text-lg">
-          <div className="flex w-full items-center">
-            <div className="relative flex w-16">
-              <div className="h-8 w-8">
+        <div className={styles.liquidityContainer}>
+          <div className={styles.liquidityPosition}>
+            <div className={styles.imagesWrapper}>
+              <div className={styles.imageContainer}>
                 <Image src={token.icon} alt={token.symbol} fill="contain" />
               </div>
-              <div className="absolute left-5 h-8 w-8">
+              <div className={`absolute left-5 ${styles.imageContainer}`}>
                 <Image src={ethLogo} alt="ETH" fill="contain" />
               </div>
             </div>
-            <div className="mr-14 tracking-wider">{token.symbol}-ETH</div>
-            <div className="font-medium tracking-wider">
+            <div className={styles.liquidityPair}>{token.symbol}-ETH</div>
+            <div className={styles.liquidityValue}>
               {formatPrecision(LPtokens, 5)}
             </div>
-            <div className=" ml-4 rounded-lg border border-[#20242A] bg-[#50515265] py-1 px-2 text-sm font-semibold tracking-wider ">
+            <div className={styles.shareValue}>
               {formatPrecision(poolShare, 3)}%
             </div>
-            <div className={styles.share}>share</div>
+            <div className={styles.shareLabel}>share</div>
           </div>
           <div>
-            <button className="rounded-lg bg-[#0e3f85] px-2 py-1 text-sm hover:bg-[#185dbe]">
-              Remove
-            </button>
+            <button className={styles.removeButton}>Remove</button>
           </div>
         </div>
       ) : (
-        <div className="text-md flex w-full justify-center py-3 tracking-wider text-[#3079df]">
-          You have no position yet!
-        </div>
+        <div className={styles.noPositionText}>You have no position yet!</div>
       )}
     </div>
   )
--- client/components/Liquidity/LiquidityPosition/styles.js
+++ client/components/Liquidity/LiquidityPosition/styles.js
@@ -1,5 +1,14 @@
 export const styles = {
   container: `bg-[#191B1F] w-[40rem] rounded-2xl p-4`,
   header: `px-2 mb-6 flex items-center justify-center font-medium text-xl pb-2 border-b border-gray-800`,
-  share: `text-sm ml-1 text-gray-400`,
+  liquidityContainer: `my-3 flex justify-between rounded-2xl border border-[#20242A]  bg-[#20242A] p-6 text-lg`,
+  liquidityPosition: `flex w-full items-center`,
+  liquidityPair: `mr-14 tracking-wider`,
+  liquidityValue: `font-medium tracking-wider`,
+  imagesWrapper: `relative flex w-16`,
+  imageContainer: `h-8 w-8`,
+  shareValue: ` ml-4 rounded-lg border border-[#20242A] bg-[#50515265] py-1 px-2 text-sm font-semibold tracking-wider`,
+  shareLabel: `text-sm ml-1 text-gray-400`,
+  removeButton: `rounded-lg bg-[#0e3f85] px-2 py-1 text-sm hover:bg-[#185dbe]`,
+  noPositionText: `text-md flex w-full justify-center py-3 tracking-wider text-[#3079df]`,
 }
