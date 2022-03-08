3d69f967f1ffd25332fa87da66f5af8a76b4596f
gratestas <stasgrate@gmail.com>
2022-03-08 16:29:18 +0300

    added unit conversion functions for tokens

--- client/smart_contract/lib/utils.js
+++ client/smart_contract/lib/utils.js
@@ -20,12 +20,21 @@ export const toWei = (value) => utils.parseEther(value.toString())
 export const fromWei = (value) =>
   utils.formatEther(typeof value === 'string' ? value : value.toString())
 
+export const parseUnits = (value, decimals) =>
+  utils.parseUnits(
+    typeof value === 'string' ? value : value.toString(),
+    decimals
+  )
+export const formatUnits = (value, decimals) =>
+  utils.formatUnits(value, decimals)
+
 export const getBalance = async (address) => {
   const provider = getProvider(eth)
   return await provider.getBalance(address)
 }
 
 export const formatPrecision = (value, decimal = 7) => {
-  const numberValue = Number(value).toPrecision(decimal)
-  return numberValue.toString()
+  return value % 1
+    ? Number.parseFloat(value).toFixed(decimal)
+    : Number.parseFloat(value).toFixed(0)
 }
