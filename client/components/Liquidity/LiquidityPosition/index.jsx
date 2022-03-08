cbcbc7e117f7619fb8da8368c1e73244dd0925e2
gratestas <stasgrate@gmail.com>
2022-03-08 16:33:50 +0300

    added removeLiquidity handle function as passed argument and updated rendering condition.LP  position container  will not be displayed if no token selected

--- client/components/Liquidity/LiquidityPosition/index.jsx
+++ client/components/Liquidity/LiquidityPosition/index.jsx
@@ -3,11 +3,17 @@ import { styles } from './styles'
 import ethLogo from '../../../assets/eth.png'
 import { formatPrecision } from '../../../smart_contract/lib/utils'
 
-const LuqiudityPosition = ({ token, LPtokens, poolShare }) => {
+const LuqiudityPosition = ({
+  token,
+  LPtokens,
+  poolShare,
+  removeLuqidityHandler,
+}) => {
+  const doesUserHaveLPtokens = Number(LPtokens)
   return (
     <div className={`${styles.container} mt-10`}>
       <div className={styles.header}>Your Liquidity</div>
-      {LPtokens ? (
+      {doesUserHaveLPtokens && token ? (
         <div className={styles.liquidityContainer}>
           <div className={styles.liquidityPosition}>
             <div className={styles.imagesWrapper}>
@@ -20,15 +26,20 @@ const LuqiudityPosition = ({ token, LPtokens, poolShare }) => {
             </div>
             <div className={styles.liquidityPair}>{token.symbol}-ETH</div>
             <div className={styles.liquidityValue}>
-              {formatPrecision(LPtokens, 5)}
+              {formatPrecision(LPtokens, 2)}
             </div>
             <div className={styles.shareValue}>
-              {formatPrecision(poolShare, 3)}%
+              {formatPrecision(poolShare, 1)}%
             </div>
             <div className={styles.shareLabel}>share</div>
           </div>
           <div>
-            <button className={styles.removeButton}>Remove</button>
+            <button
+              className={styles.removeButton}
+              onClick={removeLuqidityHandler}
+            >
+              Remove
+            </button>
           </div>
         </div>
       ) : (
