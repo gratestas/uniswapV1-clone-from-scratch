2232b6608b6c1c6fb68b7deb76a35c7899d0f7d9
gratestas <stasgrate@gmail.com>
2022-03-08 16:20:31 +0300

    removed unnecassary code

--- client/context/TransactionContext.js
+++ client/context/TransactionContext.js
@@ -35,20 +35,6 @@ export const TransactionProvider = ({ children }) => {
   const loadBlockchainData = async (metamask = eth) => {
     const signer = getSigner(metamask)
     setFactory(getFactoryContract(signer))
-    const token = getTokenContract(
-      '0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512',
-      signer
-    )
-    console.log({ token })
-    const exchangeUSDT = await getExchange(token.address, signer)
-    console.log({ exchangeUSDT })
-    await token.approve(exchangeUSDT.address, toWei(20000))
-    await exchangeUSDT.addLiquidity(toWei(20000), { value: toWei(5) })
-    console.log(`
-    liquidiy has been added
-    =======================
-    ETH balance of exchange: ${fromWei(await getBalance(exchangeUSDT.address))}
-    `)
   }
 
   const closePanel = () => {
