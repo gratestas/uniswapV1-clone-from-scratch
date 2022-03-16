5c5799934c9145b43e61b37f9b911ff1e388b211
gratestas <stasgrate@gmail.com>
2022-03-16 14:29:59 +0300

    removed button

--- client/pages/pools/index.js
+++ client/pages/pools/index.js
@@ -1,10 +1,8 @@
 import { useEffect, useState } from 'react'
-import Link from 'next/link'
 import { getAllPoolsData } from '../../smart_contract/lib/liquidity'
 
 import PoolTable from '../../components/PoolTable'
 
-const styles = {}
 const Pools = () => {
   const [allPoolsData, setAllPoolsData] = useState()
 
@@ -13,14 +11,7 @@ const Pools = () => {
     setAllPoolsData(_allPoolsData)
   }, [])
   return (
-    <div className="flex flex-col items-center space-y-5">
-      <div className="flex w-full justify-end">
-        <div className=" divide-x rounded-md bg-[#191B1F] py-2 px-4 font-medium tracking-wide text-[#f0a912] hover:bg-[#113f83] hover:text-white">
-          <Link href="liquidity">
-            <a>new pool</a>
-          </Link>
-        </div>
-      </div>
+    <div className="flex flex-col items-center">
       <PoolTable poolsData={allPoolsData} />
     </div>
   )
