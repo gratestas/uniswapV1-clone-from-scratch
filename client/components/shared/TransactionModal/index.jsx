9622551aea0941bd6333f779353b516f30eb61d0
gratestas <stasgrate@gmail.com>
2022-03-16 13:23:42 +0300

    changed styling

--- client/components/shared/TransactionModal/index.jsx
+++ client/components/shared/TransactionModal/index.jsx
@@ -1,18 +1,18 @@
 import TransactionLoader from '../TransactionLoader'
 
 const styles = {
-  wrapper: `absolute inset-x-auto z-20 flex top-10 h-96 w-[40rem] flex-col justify-between rounded-2xl bg-[#191B1F] py-10 px-20 text-center`,
+  wrapper: `absolute inset-x-auto z-20 flex top-0 left-6 h-96 w-[35rem] flex-col justify-between items-center rounded-2xl bg-[#191B1F] text-center`,
 }
 
 const TransactionModal = ({ isOpen, isLoading, message }) => {
   return (
-    <div>
+    <>
       {isOpen && (
         <div className={styles.wrapper}>
           <TransactionLoader message={message} isLoading={isLoading} />
         </div>
       )}
-    </div>
+    </>
   )
 }
 
