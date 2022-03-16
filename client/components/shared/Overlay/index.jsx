1c78a497f956b8f319ad778ac0f2476c50e6d344
gratestas <stasgrate@gmail.com>
2022-03-16 13:22:00 +0300

    changed bg color

--- client/components/shared/Overlay/index.jsx
+++ client/components/shared/Overlay/index.jsx
@@ -1,6 +1,6 @@
 const Overlay = () => {
   return (
-    <div className="absolute inset-0 bg-slate-900/70 transition duration-500 "></div>
+    <div className="absolute inset-0 bg-black/60 transition duration-500 "></div>
   )
 }
 
