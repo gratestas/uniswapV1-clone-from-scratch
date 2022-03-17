f198db9bd68019eaeeaf71ad5ce74186fd7add03
gratestas <stasgrate@gmail.com>
2022-03-17 17:17:37 +0300

    changed styles

--- client/components/PoolTable/styles.js
+++ client/components/PoolTable/styles.js
@@ -4,7 +4,7 @@ export default {
   thead: ``,
   thead_item: `first:text-left text-right first:w-[40rem] py-3 mx-6 text-gray-400 font-normal text-lg tracking-wide`,
   tbody: `divide-y divide-gray-800`,
-  tbody_row: `my-2 mt-2 text-center hover:text-[#f0aa12c5]`,
+  tbody_row: `my-2 mt-2 text-center text-[#cfcfcf] hover:text-white hover:bg-[#1b1d21]`,
   tbody_item: `first:text-left text-right py-4 font-medium tracking-wider text-lg `,
   tbody_item__inner: `relative flex items-center`,
 }
