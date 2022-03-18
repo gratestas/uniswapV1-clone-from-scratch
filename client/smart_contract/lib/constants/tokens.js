87162b70645b6d7aa6c71013d67bda94ecb5f764
gratestas <stasgrate@gmail.com>
2022-03-18 11:24:44 +0300

    deployed factory contract  and token contracts on rinkeby network

--- client/smart_contract/lib/constants/tokens.js
+++ client/smart_contract/lib/constants/tokens.js
@@ -15,24 +15,24 @@ export default [
     name: 'Tether',
     symbol: 'USDT',
     icon: tether,
-    address: '0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512',
+    address: '0xf8d6f6b94eAE4C0e2CeCaB887b278c4A374324E6',
   },
   {
     name: 'Balancer',
     symbol: 'BAL',
     icon: balancer,
-    address: '0xDc64a140Aa3E981100a9becA4E685f962f0cF6C9',
+    address: '0x5d8359478C33962d412bf1ecea8204d301A066e6',
   },
   {
     name: 'Chainlink',
     symbol: 'LINK',
     icon: chainlink,
-    address: '0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0',
+    address: '0xdC322c931351b6784ad0Ff61Ee234E37C93D4F20',
   },
   {
     name: 'Compound',
     symbol: 'COMP',
     icon: compound,
-    address: '0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9',
+    address: '0x84046B157005AcAabc4164AcBC648143Cd3a126e',
   },
 ]
